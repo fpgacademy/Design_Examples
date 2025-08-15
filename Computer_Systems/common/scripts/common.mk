ed4575d69b9861eb65022e7ad2e74d165161e2bd
FPGAcademyDev <85641050+FPGAcademyDev@users.noreply.github.com>
2025-08-15 15:49:36 -0400

    Updating makefile to run under Linux
    Previous version only worked on WSL

--- Computer_Systems/common/scripts/common.mk
+++ Computer_Systems/common/scripts/common.mk
@@ -1,3 +1,9 @@
+# Check if the Makefile is running on WSL or directly in Linux
+ifeq ($(origin WSL_DISTRO_NAME),environment)
+    EXE := .exe
+else
+    EXE := 
+endif
 
 QSYSOBJS = $(TCLSRCS:.tcl=.qsys)
 SOPCOBJS = $(QSYSSRC:.qsys=.sopcinfo)
@@ -14,23 +20,23 @@ generate_qsys_files: $(QSYSOBJS)
 %.tcl: # dummy make target
 	
 %.qsys: %.tcl
-	qsys-script.exe --script=$(S2CPATH)/$< > $(CURPATH)/o_$<.txt 2>&1
+	qsys-script$(EXE) --script=$(S2CPATH)/$< > $(CURPATH)/o_$<.txt 2>&1
 
 run_platform_designer: $(QSYSSRC)
 	mkdir -p $(DSTPATH)
 	cp *.qsys $(DSTPATH)
-	cd $(DSTPATH) && qsys-generate.exe ./$< --synthesis=VERILOG > $(CURPATH)/o_$<.txt 2>&1
+	cd $(DSTPATH) && qsys-generate$(EXE) ./$< --synthesis=VERILOG > $(CURPATH)/o_$<.txt 2>&1
 
 reduce_sopcinfo:
 	cd $(DSTPATH) && python3 $(D2CPATH)/strip_info.py Computer_System.sopcinfo
 
 run_quartus: $(QP_NAME)
 	cp -r $(SRCPATH) $(DSTPATH)
-	cd $(DSTPATH) && quartus_sh.exe --64bit --flow compile $< > $(CURPATH)/o_$<.txt 2>&1
+	cd $(DSTPATH) && quartus_sh$(EXE) --64bit --flow compile $< > $(CURPATH)/o_$<.txt 2>&1
 
 generate_rbf: $(QP_NAME)
 ifeq ($(GEN_RBF), 1)
-	cd $(DSTPATH) && quartus_cpf.exe -m FPP -o bitstream_compression=on -c $<.sof $<.rbf > $(CURPATH)/o_$<.rbf.txt 2>&1
+	cd $(DSTPATH) && quartus_cpf$(EXE) -m FPP -o bitstream_compression=on -c $<.sof $<.rbf > $(CURPATH)/o_$<.rbf.txt 2>&1
 endif
 
 release:
