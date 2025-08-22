740bf3e2063826f003f03d5a7559fe04c8f7bae9
FPGAcademy <74740408+fpgacademy@users.noreply.github.com>
2025-08-22 10:25:26 -0400

    Small update to comments
    Fixed reference to Nios V Command Shell, as it should have been the Nios II Command Shell

--- Computer_Systems/DE10-Standard/scripts/NiosVm.mk
+++ Computer_Systems/DE10-Standard/scripts/NiosVm.mk
@@ -1,5 +1,5 @@
-# This Makefile generates and compiles the DE10_Standard Nios V/m Computer
-# To be run in the Nios V Command shell
+# This Makefile generates and compiles the DE10-Standard Nios V/m Computer
+# To be run in the Nios II Command shell
 
 TCLSRCS = gen_audio_subsystem.tcl gen_char_buf_subsystem.tcl gen_vga_subsystem.tcl gen_video_in_edge_detection_subsystem.tcl gen_video_in_subsystem.tcl gen_niosvm_computer_system.tcl
 QSYSSRC = Computer_System.qsys
