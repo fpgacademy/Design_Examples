11b22108d30fe29fecac2e8cd36a1f59c2c0e840
FPGAcademy <74740408+fpgacademy@users.noreply.github.com>
2025-08-22 00:57:56 -0400

    Adding computer system documentation

---
+++ Computer_Systems/DE1-SoC/docs/NiosII/doc.mk
@@ -0,0 +1,6 @@
+# This Makefile compiles the computer system documentation in this folder
+
+CSROOT = ../../..
+TEXFILES = DE1-SoC_Computer_NiosII.tex
+
+include $(CSROOT)/common/scripts/doc.common.mk
---
+++ Computer_Systems/DE10-Nano/docs/ARM_A9/doc.mk
@@ -0,0 +1,6 @@
+# This Makefile compiles the computer system documentation in this folder
+
+CSROOT = ../../..
+TEXFILES = DE10-Nano_Computer_ARM.tex
+
+include $(CSROOT)/common/scripts/doc.common.mk
